2742c7f2aa4556602a4c8c7fc83acef8c5aa429a
dcs <dave@st.germa.in>
2023-03-13 17:16:21 +0000

    not needed
    FossilOrigin-Name: d4f42106e9afa641dc48fecd9c821a68b05430119617ce72f8e1631c4720a0d3

--- nostr_relay/alembic/env.py
+++ nostr_relay/alembic/env.py
@@ -24,7 +24,6 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from nostr_relay.storage import get_metadata
-from nostr_relay import auth, verification
 
 target_metadata = get_metadata()
 
